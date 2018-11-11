54ecbd885098b1acb162b103da46c502f204d19e
András Parditka <andraaspar@gmail.com>
2018-11-11 16:06:10 +0100

    Updates

---
+++ precache-manifest.55fad86c71c9ef343b3b3d7dc4499733.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd5dcdfcda0319465498",
+    "url": "/meg/static/js/runtime~main.bd5dcdfc.js"
+  },
+  {
+    "revision": "a94299d0c30883e290cc",
+    "url": "/meg/static/js/main.a94299d0.chunk.js"
+  },
+  {
+    "revision": "daae0d4e3da43b774c34",
+    "url": "/meg/static/js/1.daae0d4e.chunk.js"
+  },
+  {
+    "revision": "daae0d4e3da43b774c34",
+    "url": "/meg/static/css/1.d06cd5bf.chunk.css"
+  },
+  {
+    "revision": "ec9bb51851775ba4e02de004418ef5b5",
+    "url": "/meg/index.html"
+  }
+];
